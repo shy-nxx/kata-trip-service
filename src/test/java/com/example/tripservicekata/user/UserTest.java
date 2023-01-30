3dc22f871632c707fa8ff677dff8cb56b07b17cd
shy-nxx <20shinee@gmail.com>
2023-01-30 21:00:35 +0900

    [test] Add UserTest

--- src/test/java/com/example/tripservicekata/user/UserTest.java
+++ src/test/java/com/example/tripservicekata/user/UserTest.java
@@ -1,5 +1,43 @@
 package com.example.tripservicekata.user;
 
+import com.example.tripservicekata.trip.Trip;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
 
+    private User loggedUser = new User();
+
+    @Mock
+    private Trip trip;
+
+    @DisplayName("친구 추가하기")
+    @Test
+    void addFriendsTest() {
+        // given
+        User friend = new User();
+
+        // when
+        loggedUser.addFriend(friend);
+
+        // then
+        assertEquals(1, loggedUser.getFriends().size());
+    }
+
+    @DisplayName("여행지 추가하기")
+    @Test
+    void addTripsTest() {
+        // given
+        trip = new Trip("taiwan");
+
+        // when
+        loggedUser.addTrip(trip);
+        
+        // then
+        assertEquals(1, loggedUser.trips().size());
+    }
 }
+
